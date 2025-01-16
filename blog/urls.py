ff98926d21446e7b3fdb1ab12c0bf08eabc7dc49
Shaun Anderton <shaunanderton1@gmail.com>
2025-01-16 01:42:25 +0000

    add post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
 	path('', views.PostList.as_view(), name='home'),
+    #add a path to our post detail using the slug to identify which posts
+	path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
